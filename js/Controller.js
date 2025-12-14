cfebdd2fb0fef14c91ec4099211eff6a08733f5a
TeresaPC <teres4n@email.cz>
2025-12-14 18:51:03 +0100

    Notifikace

--- js/Controller.js
+++ js/Controller.js
@@ -7,10 +7,22 @@ class Controller {
         this.#model = model;
         this.#view = view;
 
+        this.onMedListChanged(this.#model.getMedications());
+
         this.#view.bindNavigation(this.handleNavigation);
+        this.#view.bindToggleTaken(this.handleToggleTaken);
+        this.#view.bindDeleteOrEdit(this.handleDelete, this.handleEdit);
+        this.#view.bindFormSubmit(this.handleSubmit);
+        this.#view.bindAddTimer();
+        this.#view.initDialogListeners(); 
+
+        this.#model.bindDataChanged(this.onMedListChanged);
+
+        this.#checkNotifPermission();
+        setInterval(() => this.#checkTimeLoop(), 30000);
+
 
     }
-
 
     onMedListChanged = (meds) => {
         this.#view.render(meds, this.#activePage);
@@ -20,4 +32,52 @@ class Controller {
         this.#activePage = pageId;
         this.#view.render(this.#model.getMedications(), pageId);
     }
+
+    handleToggleTaken = (id, time) => {
+        const today = new Date().toISOString().split('T')[0];
+        this.#model.toggleStatus(id, time, today);
+    }
+
+    handleDelete = (id) => {
+        if(confirm('Smazat?')) {
+            this.#model.remove(id);
+        }
+    }
+
+    handleEdit = (id) => {
+        const med = this.#model.getById(id);
+        this.#view.openDialog(med);
+    }
+
+    handleSubmit = (data) => {
+        this.#model.addOrUpdate(data.name, data.dosage, data.times, data.id);
+    }
+
+    handleNotifReq = () => {
+        Notification.requestPermission().then(p => {
+            if(p === 'granted') {
+                new Notification('MediPlan', {body: 'Zapnuto!'});
+                this.#checkNotifPermission();
+            }
+        });
+    }
+
+    #checkNotifPermission() {
+        if(!("Notification" in window)) return;
+        this.#view.showNotifBtn(Notification.permission === 'default');
+    }
+
+    #checkTimeLoop() {
+        if(Notification.permission !== 'granted') return;
+        const now = new Date();
+        const timeStr = `${String(now.getHours()).padStart(2,'0')}:${String(now.getMinutes()).padStart(2,'0')}`;
+        const today = now.toISOString().split('T')[0];
+
+        this.#model.getMedications().forEach(med => {
+            if(med.times.includes(timeStr) && !med.isTakenAt(today, timeStr)) {
+                new Notification(`Čas na léky: ${med.name}`, { body: `Dávka: ${med.dosage}` });
+            }
+        });
+    }
+
 }
